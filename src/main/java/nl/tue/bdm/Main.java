dbf2dcc7d3471e0d091e899378da4a801b9197dc
Tristan <90190432+Surrael@users.noreply.github.com>
2025-02-27 08:41:05 +0100

    Change uid type to Integer

--- src/main/java/nl/tue/bdm/Main.java
+++ src/main/java/nl/tue/bdm/Main.java
@@ -22,9 +22,9 @@ public class Main {
 
     // We create pairs (userID, (1, rating)), with the 1 being used to keep track of
     // the amount of ratings a user did
-    JavaPairRDD<String, Tuple2<Integer, Integer>> pairs = linesWithRating.mapToPair(s -> {
+    JavaPairRDD<Integer, Tuple2<Integer, Integer>> pairs = linesWithRating.mapToPair(s -> {
       String[] words = s.split(",");
-      String uid = words[0];
+      Integer uid = Integer.valueOf(words[0]);
       Integer rating = Integer.valueOf(words[2]);
       return new Tuple2<>(uid, new Tuple2<>(1, rating));
 
@@ -32,7 +32,7 @@ public class Main {
 
     // Sum the counts and the ratings for each user, so we have per userID:
     // (userID, countOfRatings, sumOfRatings)
-    JavaPairRDD<String, Tuple2<Integer, Integer>> countWithSumOfRatings = pairs
+    JavaPairRDD<Integer, Tuple2<Integer, Integer>> countWithSumOfRatings = pairs
         .reduceByKey((pair1, pair2) -> {
           Integer count = pair1._1 + pair2._1;
           Integer rating = pair1._2 + pair2._2;
@@ -40,10 +40,10 @@ public class Main {
         });
 
     // Compute avg rating per user: (userID, avgRating)
-    JavaPairRDD<String, Integer> avgUserRatings = countWithSumOfRatings.mapValues(pair -> (pair._2 / pair._1));
+    JavaPairRDD<Integer, Integer> avgUserRatings = countWithSumOfRatings.mapValues(pair -> (pair._2 / pair._1));
 
     // Find the user with the highest avg rating
-    String maxUser = avgUserRatings.reduce((u1, u2) -> u1._2 > u2._2 ? u1 : u2)._1;
+    Integer maxUser = avgUserRatings.reduce((u1, u2) -> u1._2 > u2._2 ? u1 : u2)._1;
 
     System.out.println(maxUser);
 
