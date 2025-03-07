725f97f18ef5617145dea704d9e5280aeb3cfad6
Veerle <veerle@furst.nl>
2025-03-07 13:05:32 +0100

    For vscode on windows :(

--- src/main/java/nl/tue/bdm/Main.java
+++ src/main/java/nl/tue/bdm/Main.java
@@ -1,5 +1,7 @@
 package nl.tue.bdm;
 
+import java.io.IOException;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -20,6 +22,13 @@ public class Main {
     JavaRDD<String> lines = spark.read().textFile("plays.csv").javaRDD();
     lines.take(10).forEach(System.out::println);
 
+    System.out.println("Press ENTER to close...");
+    try {
+      System.in.read();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     spark.stop();
   }
 }
