9ffe60345008d2b6f3586538b50704257edc26da
Veerle <veerle@furst.nl>
2025-03-10 14:39:56 +0100

    Question 6

--- src/main/java/nl/tue/bdm/Main.java
+++ src/main/java/nl/tue/bdm/Main.java
@@ -2,10 +2,13 @@ package nl.tue.bdm;
 
 import java.io.IOException;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
 
 public class Main {
   public static void main(String[] args) {
@@ -22,11 +25,32 @@ public class Main {
     JavaRDD<String> lines = spark.read().textFile("plays.csv").javaRDD();
     lines.take(10).forEach(System.out::println);
 
+
+    // Write code in Spark to find the id of the song that was played the most times in the dataset. 
+    // If more than one such song exists with an equal number of plays, return all of them.
+
+    // Get tuples of (songID, 1)
+    JavaPairRDD<Integer, Integer> songs = lines.mapToPair(x -> new Tuple2<Integer, Integer>(Integer.valueOf(x.split(",")[1]), 1));
+    songs.take(10).forEach(System.out::println);
+
+    // Add up the amount of times the song was listened to
+    JavaPairRDD<Integer, Integer> songAmount = songs.reduceByKey((a,b) -> a + b);
+    songAmount.take(10).forEach(System.out::println);
+    JavaPairRDD<Integer, Integer> songAmountMax = songAmount;
+
+    // Get the max
+    Integer maxAmount = songAmountMax.reduce((a,b) -> { if (a._2 > b._2) {return a;} else {return b;} })._2;
+    System.out.println(maxAmount);
+
+    // Get the values that match the max
+    JavaPairRDD<Integer, Integer> result = songAmount.filter(a -> a._2 == maxAmount);
+    result.take(10).forEach(System.out::println);
+
+
     System.out.println("Press ENTER to close...");
     try {
       System.in.read();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     spark.stop();
