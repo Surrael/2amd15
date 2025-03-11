56ba91eb20c75a4ec89205840ff5072eebb0e9e0
Veerle <veerle@furst.nl>
2025-03-11 10:31:45 +0100

    comment

--- src/main/java/nl/tue/bdm/Main.java
+++ src/main/java/nl/tue/bdm/Main.java
@@ -7,6 +7,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
 
@@ -22,11 +25,19 @@ public class Main {
     JavaRDD<String> lines = spark.read().textFile("plays.csv").javaRDD();
     lines.take(10).forEach(System.out::println);
 
+    /***
+     * You want to build a recommendation model with Spark, for recommending new songs to users based on their ratings on past songs. 
+     * There are many ways to answer this question, as well as many definitions on what would constitute a good recommendation. 
+     * You are free to choose, and experiment with different ones. One possible approach is to model the users/songs ratings as a sparse matrix, 
+     * and then run classical data mining/machine learning algorithms, such as K-Means or K-nearest neighbor, to find possible answers. 
+     * Mllib is a Spark library that can be used for this exercise.
+    ***/
+
+
     System.out.println("Press ENTER to close...");
     try {
       System.in.read();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     spark.stop();
