660c6c59cfd41902de975c7315a5ee5cfe0d0ad5
Veerle <veerle@furst.nl>
2025-03-18 17:32:48 +0100

    Q7

---
+++ src/main/java/nl/tue/bdm/CountMinSketch.java
@@ -0,0 +1,106 @@
+package nl.tue.bdm;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Random;
+
+public class CountMinSketch implements Serializable {
+  private int[][] data;
+  private static int rows;
+  private static int width;
+
+  // hash function constants
+  private static int[] a;
+  private static int[] b;
+  private static int[] p;
+
+  public CountMinSketch() {
+    data = new int[rows][width];
+  }
+
+  // Initialize the rows, width, and hash functions
+  public static void init(int r, int w) {
+    rows = r;
+    width = w;
+    initHashFunctions(rows);
+  }
+
+  // Initialize hash functions
+  public static void initHashFunctions(int rows) {
+    a = new int[rows];
+    b = new int[rows];
+    p = new int[rows];
+
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++) {
+      a[i] = rand.nextInt(Integer.MAX_VALUE);
+      b[i] = rand.nextInt(Integer.MAX_VALUE);
+      p[i] = BigInteger.probablePrime(31, rand).intValue();
+    }
+  }
+
+  // Merge two sketches
+  public static CountMinSketch merge(CountMinSketch sketch1, CountMinSketch sketch2) {
+    CountMinSketch result = new CountMinSketch();
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < width; j++) {
+        result.data[i][j] = sketch1.data[i][j] + sketch2.data[i][j];
+      }
+    }
+    return result;
+  }
+
+  // Add a value to the sketch
+  public void add(int value) {
+    for (int i = 0; i < rows; i++) {
+      int hash = hash(value, i);
+      data[i][hash]++;
+    }
+  }
+
+  // Get the frequence of the value
+  public int getFreq(int value) {
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < rows; i++) {
+      int hash = hash(value, i);
+      if (data[i][hash] < min)
+        min = data[i][hash];
+    }
+    return min;
+  }
+
+  // Hash a value
+  private int hash(int value, int row) {
+    // h(x) = ((a * x + b) % p) % width
+    long intermediate = (long) a[row] * value + b[row]; // prevent overflow
+    return (int) ((intermediate % p[row] + p[row]) % p[row] % width);
+
+  }
+
+  // To print the sketch
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("CountMinSketch(rows=").append(rows).append(", width=").append(width).append(")\n");
+
+    // Print data array
+    sb.append("Data:\n");
+    for (int i = 0; i < rows; i++) {
+      sb.append("Row ").append(i).append(": ");
+      for (int j = 0; j < width; j++) {
+        sb.append(data[i][j]).append(" ");
+      }
+      sb.append("\n");
+    }
+
+    // Print hash function constants
+    sb.append("Hash Function Constants:\n");
+    for (int i = 0; i < rows; i++) {
+      sb.append("Row ").append(i).append(": a=").append(a[i])
+          .append(", b=").append(b[i])
+          .append(", p=").append(p[i]).append("\n");
+    }
+
+    return sb.toString();
+  }
+}
--- src/main/java/nl/tue/bdm/Main.java
+++ src/main/java/nl/tue/bdm/Main.java
@@ -1,12 +1,15 @@
 package nl.tue.bdm;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import scala.Tuple2;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
 public class Main {
   public static void main(String[] args) {
 
@@ -16,17 +19,63 @@ public class Main {
         .master("local[*]")
         .getOrCreate();
 
-    Dataset<Row> df = spark.read().csv("plays.csv");
-    df.show(10);
-
+    // Read the file
     JavaRDD<String> lines = spark.read().textFile("plays.csv").javaRDD();
     lines.take(10).forEach(System.out::println);
+
+    // Only get the IDs
+    JavaRDD<Integer> songIDs = lines.map(line -> Integer.valueOf(line.split(",")[1].trim()));
+    songIDs.take(10).forEach(System.out::println);
+
+    // Initalize rows/columns and the hash functions for each row
+    // int rows = (int) Math.ceil(2 / epsilon);
+    // int width = (int) Math.ceil(-Math.log(1 - confidence) / Math.log(2)) * 1000;
+    int rows = 100;
+    int width = 100;
+    CountMinSketch.init(rows, width);
+
+    // For the partitions we add it to a sketch
+    JavaRDD<CountMinSketch> localSketches = songIDs.mapPartitions(partition -> {
+      CountMinSketch localSketch = new CountMinSketch();
+      while (partition.hasNext())
+        localSketch.add(partition.next());
+      return Arrays.asList(localSketch).iterator();
+    });
+
+    // Combine the sketches
+    CountMinSketch totalSketch = localSketches.reduce((sketch1, sketch2) -> {
+      return CountMinSketch.merge(sketch1, sketch2);
+    });
+
+    System.out.println(totalSketch);
+
+    // Find the actual amount of plays for each song
+    JavaPairRDD<Integer, Integer> songIDCount = songIDs.mapToPair(songId -> new Tuple2<Integer, Integer>(songId, 1));
+    songIDCount.take(10).forEach(System.out::println);
+    JavaPairRDD<Integer, Integer> songIDSum = songIDCount.reduceByKey((a,b) -> a+b);
+    songIDSum.take(10).forEach(System.out::println);
+
+    // The ID to estimate
+    int songid = 63829;
+
+    // Get the actual value
+    List<Integer> counts = songIDSum.lookup(songid);
+    int actual = counts.isEmpty() ? 0 : counts.get(0);
+    // Get the estimate
+    int estimate = totalSketch.getFreq(songid);
+
+    // Print result (actual vs. estimate)
+    System.out.println("Actual Count: " + actual);
+    System.out.println( "Estimate: " + estimate);
+
+    // Calculate the error
+    int error = ((estimate + actual) / actual) *100;
+    System.out.println( "Error: " + error);
 
     System.out.println("Press ENTER to close...");
     try {
       System.in.read();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     spark.stop();
